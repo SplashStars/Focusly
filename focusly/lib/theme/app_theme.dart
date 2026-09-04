eff1e0cb3545faaa539778bbd697473e52b1b2d4
SplashStars <magnnadi@gmail.com>
2026-09-04 07:57:33 +0100

    Flutter 3.38: cardTheme now takes CardThemeData

--- focusly/lib/theme/app_theme.dart
+++ focusly/lib/theme/app_theme.dart
@@ -104,7 +104,7 @@ class AppTheme {
           fontWeight: FontWeight.w600,
         ),
       ),
-      cardTheme: CardTheme(
+      cardTheme: CardThemeData(
         color: AppColors.surface,
         elevation: 0,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
